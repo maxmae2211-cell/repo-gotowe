0c0118d286c1262f2229732f8ab32a591c9e6283
maxmae2211-cell <maxmae2211@gmail.com>
2026-04-13 05:57:22 +0200

    Clean repo and update .gitignore

--- tests/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request
-import time
-
-app = Flask(__name__)
-
-@app.before_request
-def log_request():
-    print(f"[REQ] {time.time()} {request.method} {request.path} {request.data}")
